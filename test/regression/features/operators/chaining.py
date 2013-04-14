faedd60d75c8870ac465c0f1a176764b3a9959d6
Bernie Pope <florbitous@gmail.com>
2013-04-14 16:17:10 +1000

    Added more cases to the comparison operator chaining test

--- test/regression/features/operators/chaining.py
+++ test/regression/features/operators/chaining.py
@@ -1,5 +1,12 @@
+w = 12
 x = 3
 y = 2
 z = 4
-print(x < z > y)   # should be same as (3 < 4 and 4 > 2)
+print(y < z) # no chaining, with True answer
+print(z < y) # no chaining, with False answer
+print(x < z > y)   # should be same as (3 < 4 and 4 > 2), True answer
+print(x < z > w)   # should be same as (3 < 4 and 4 > 12), False answer
 print((x < z) > y) # should be same as (3 < 4 and True > 2)
+print(y < x < z < w) # 5-chain True answer
+print(y < z < x < w) # 5-chain False answer
+print(y < z < x + 2 < w) # 5-chain True answer, nested expression
