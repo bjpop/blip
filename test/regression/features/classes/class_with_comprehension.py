35076d66fbe6ea479942034cd99b7583bb199b58
Bernie Pope <florbitous@gmail.com>
2013-05-24 19:10:07 +1000

    Test case for a comprehension in the body of a class

---
+++ test/regression/features/classes/class_with_comprehension.py
@@ -0,0 +1,18 @@
+class C():
+   l = [1,2,3]
+   s = [ y + 1 for y in l]
+
+print(C.l)
+print(C.s)
+
+# We treat it the same as below. The problem is that
+# the variable l is not in scope in the definition of x()
+# because of the unusual scope rules in class bodies
+#class C():
+#   l = [1,2,3]
+#   def x():
+#       result = []
+#       for y in l:
+#           result.append(y)
+#       return result
+#   s = x() 
